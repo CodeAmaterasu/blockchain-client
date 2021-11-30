34ccc896fd29e6a78bc24ce150e60ec217485acd
Jakob Danilo <danilo.jakob@wuerth-itensis.com>
2021-11-30 10:12:05 +0100

    CHANGE: Implemented setup option for creating appdata directory. CHANGE: Implemented the ability to switch the current used wallet

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import argparse
 import base64
+import configparser
+import os
 
 from core.blockchain_sdk import BlockChainClient
 from core.blockchain_sdk import Block
@@ -17,10 +19,35 @@ def __register_arguments():
     parser.add_argument('-gb', '--get-blockchain', action='store_true', help='Get blockchain')
     parser.add_argument('-cb', '--create-block', action='store_true', help='Create new block on the blockchain')
     parser.add_argument('-cw', '--create-wallet', action='store_true', help='Create a new wallet for the blockchain')
+    parser.add_argument('-sw', '--switch-wallet', help='Switch to new wallet. Pass wallet name as parameter')
+    parser.add_argument('-s', '--setup', action='store_true', help='First time setup')
 
 
 def __parse_arguments():
     return parser.parse_args()
+
+
+def __setup():
+    print('Configuring directories.....')
+    app_data_dir = os.getenv('APPDATA')
+    blockchain_cli_dir = app_data_dir + '\\blockchain-cli'
+    # Create script dir
+    os.mkdir(blockchain_cli_dir)
+    wallet_dir = blockchain_cli_dir + '\\wallets'
+    # Create wallet dir
+    os.mkdir(wallet_dir)
+    print('Creating configuration file......')
+    config = configparser.ConfigParser()
+    config['Wallet'] = {
+        'pub_key': '',
+        'priv_key': ''
+    }
+    config['Blockchain'] = {
+        'host': ''
+    }
+    with open(blockchain_cli_dir + '\\config.ini', 'w') as file:
+        config.write(file)
+    print('Finished setup!')
 
 
 if __name__ == '__main__':
@@ -43,11 +70,34 @@ if __name__ == '__main__':
         private_key = sk.to_string().hex()
         vk = sk.get_verifying_key()
         public_key = vk.to_string().hex()
+        # Make it shorter and therefore moros.getenv('APPDATA') + '\\blockchain-cli\\walletse readable
         public_key = base64.b64encode(bytes.fromhex(public_key))
 
-        filename = input("Write the name of your new address: ") + ".txt"
-        with open(filename, "w") as f:
-            f.write("Private key: {0}\nWallet address / Public key: {1}".format(private_key, public_key.decode()))
-        print("Your new address and private key are now in the file {0}".format(filename))
+        filename = input('Write the name of your new address: ') + '.txt'
+        file_path = os.getenv('APPDATA') + '\\blockchain-cli\\wallets\\' + filename
+        with open(file_path, 'w') as f:
+            f.write('Private key:{0}\nWallet address / Public key:{1}'.format(private_key, public_key.decode()))
+        print('Your new address and private key are now in the file {0}'.format(file_path))
+    elif args.setup:
+        print('Starting setup')
+        __setup()
+    elif args.switch_wallet:
+        available_wallets = os.listdir(os.getenv('APPDATA') + '\\blockchain-cli\\wallets')
+        for wallet in available_wallets:
+            if args.switch_wallet == wallet.split('.')[0]:
+                with open(os.getenv('APPDATA') + '\\blockchain-cli\\wallets\\' + wallet, 'r') as wallet_file:
+                    wallet_content = wallet_file.readlines()
+                    my_config = configparser.ConfigParser()
+                    my_config.read(os.getenv('APPDATA') + '\\blockchain-cli\\config.ini')
+                    for content in wallet_content:
+                        split_content = content.strip().split(':')
+                        if 'Private key' in split_content[0]:
+                            my_config['Wallet']['priv_key'] = split_content[1]
+                        elif 'Wallet address / Public key' in split_content[0]:
+                            my_config['Wallet']['pub_key'] = split_content[1]
+                    with open(os.getenv('APPDATA') + '\\blockchain-cli\\config.ini', 'w') as config_file:
+                        my_config.write(config_file)
+
+        print('Switched Wallet to: ' + args.switch_wallet)
 
 
